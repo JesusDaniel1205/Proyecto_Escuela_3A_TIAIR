1218aaf7701aed3d962a962c4d2aeefda99dcb73
JesusDaniel1205 <jesusdanielsanchez1205@gmail.com>
2025-07-17 13:02:31 -0600

    Adding CarreraDAO

--- src/App.java
+++ src/App.java
@@ -8,12 +8,14 @@ import screens.VentanaPrincipal;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        Connection conn = null;
+
         try{
-            Connection conn = Conexion.conectar();{
+             conn = Conexion.conectar();{
                 System.out.println("Conexion exitosa a la base de datos");
             }
         } catch (SQLException e){
-            System.out.println("Error al conectar l abase de datos");
+            System.out.println("Error al conectar l abase de datos" + e.getMessage());
         }
 
 
@@ -28,7 +30,7 @@ public class App {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal("Proyecto Escuela");
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal("Proyecto Escuela", conn);
         ventanaPrincipal.setSize(500,500);
         ventanaPrincipal.setVisible(true);
     }
